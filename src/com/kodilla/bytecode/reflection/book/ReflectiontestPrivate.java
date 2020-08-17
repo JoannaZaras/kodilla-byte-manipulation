78be0e2f0e4e2268a992dc98dccf1ae9d2779d2c
Joanna Zaras <joanna.zaras@gmail.com>
2020-08-17 17:51:19 +0200

    Reformatted code

--- src/com/kodilla/bytecode/reflection/book/ReflectiontestPrivate.java
+++ src/com/kodilla/bytecode/reflection/book/ReflectiontestPrivate.java
@@ -6,32 +6,32 @@ import java.lang.reflect.Method;
 
 public class ReflectiontestPrivate {
 
-   public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException,
-      NoSuchMethodException, InvocationTargetException {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException,
+            NoSuchMethodException, InvocationTargetException {
 
-      Book book = new Book("Title", "Signature", 2003);
-      System.out.println("Initial value of signature: " + book.getSignature());
-      System.out.println("Initial value of year: " + book.getYear());
+        Book book = new Book("Title", "Signature", 2003);
+        System.out.println("Initial value of signature: " + book.getSignature());
+        System.out.println("Initial value of year: " + book.getYear());
 
-      Field signatureField = Book.class.getDeclaredField("signature");
-      signatureField.setAccessible(true);
+        Field signatureField = Book.class.getDeclaredField("signature");
+        signatureField.setAccessible(true);
 
-      String value = (String)signatureField.get(book);
-      System.out.println("Value of private field signature: " + value);
+        String value = (String) signatureField.get(book);
+        System.out.println("Value of private field signature: " + value);
 
-      Method rentBookMethod = Book.class.getDeclaredMethod("rentBook", null);
-      rentBookMethod.setAccessible(true);
+        Method rentBookMethod = Book.class.getDeclaredMethod("rentBook", null);
+        rentBookMethod.setAccessible(true);
 
-      boolean result = (boolean)rentBookMethod.invoke(book);
-      System.out.println("Result of invoked private method rentBook: "  + result);
+        boolean result = (boolean) rentBookMethod.invoke(book);
+        System.out.println("Result of invoked private method rentBook: " + result);
 
-      Method setDetailsMethod
-         = Book.class.getDeclaredMethod("setDetails", new Class[]{String.class, int.class});
+        Method setDetailsMethod
+                = Book.class.getDeclaredMethod("setDetails", new Class[]{String.class, int.class});
 
-      setDetailsMethod.setAccessible(true);
-      setDetailsMethod.invoke(book, "new Signature", 1990);
-      System.out.println("Signature set via private method setDetails: " + book.getSignature());
-      System.out.println("Year set via private method setDetails: " + book.getYear());
-  }
+        setDetailsMethod.setAccessible(true);
+        setDetailsMethod.invoke(book, "new Signature", 1990);
+        System.out.println("Signature set via private method setDetails: " + book.getSignature());
+        System.out.println("Year set via private method setDetails: " + book.getYear());
+    }
 
 }
